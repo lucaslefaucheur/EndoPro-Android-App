3ac0998b48c70a841f1b85389bed3cb3b73fd7c5
mathieudugre <math.dugre@gmail.com>
2018-12-03 15:00:16 -0500

    Added moving average and change line plot colors

--- app/src/main/java/comp354/concordia/endopro/GroupF/Task2.java
+++ app/src/main/java/comp354/concordia/endopro/GroupF/Task2.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -64,19 +65,31 @@ public class Task2 extends AppCompatActivity {
         final ArrayList<String> xAxes = new ArrayList<>();
         ArrayList<Entry> yAxesTemp = new ArrayList<>();
         ArrayList<Entry> yAxesWindSpeed = new ArrayList<>();
+        ArrayList<Entry> yAxesMovingAvgTemp = new ArrayList<>();
+        ArrayList<Entry> yAxesMovingAvgWindSpeed = new ArrayList<>();
 
 //        Set data points
+        float movingAvgWindSpeed = 0;
+        float movingAvgTemp = 0;
         for (int i = 0; i < allWeatherEntities.size(); i++) {
+            // Get Entry values
             float avgTempC = allWeatherEntities.get(i).getAvgtemp_c();
             float avgWindSpeedKph = allWeatherEntities.get(i).getAvgwind_kph();
-            String dateStr = allWeatherEntities.get(i).getDate();
 
-//            Format x labels
+            // Adjust moving average
+            movingAvgTemp += avgTempC;
+            movingAvgWindSpeed += avgWindSpeedKph;
+
+            // Format x labels
+            String dateStr = allWeatherEntities.get(i).getDate();
             LocalDate date = LocalDate.parse(dateStr);
             String x = date.getMonth().name().substring(0,3) + " " +  dateStr.substring(8);
 
+            // Add entry to graph
             yAxesTemp.add(new Entry(i, (int) avgTempC));
             yAxesWindSpeed.add(new Entry(i, (int) avgWindSpeedKph));
+            yAxesMovingAvgTemp.add(new Entry(i, (int) movingAvgTemp / (i+1)));
+            yAxesMovingAvgWindSpeed.add(new Entry(i, (int) movingAvgWindSpeed / (i+1)));
             xAxes.add(i, x);
         }
 
@@ -84,28 +97,69 @@ public class Task2 extends AppCompatActivity {
 
 //        Plot style average temperature
         LineDataSet lineDataSetTemp = new LineDataSet(yAxesTemp, "Average Temperature " + getString(R.string.degreeC));
-        lineDataSetTemp.setDrawCircles(false);
-        lineDataSetTemp.setColor(Color.rgb(52, 202, 255));
+        lineDataSetTemp.setCircleColor(Color.rgb(255, 176, 144));
+        lineDataSetTemp.setColor(Color.rgb(255, 176, 144));
         lineDataSetTemp.setDrawValues(false);
         lineDataSetTemp.setLineWidth(3);
 
 //        Plot style average wind speed
         LineDataSet lineDataSetWindSpeed = new LineDataSet(yAxesWindSpeed, "Average Wind Speed Kph");
-        lineDataSetWindSpeed.setDrawCircles(false);
-        lineDataSetWindSpeed.setColor(Color.rgb(232, 35, 33));
+        lineDataSetWindSpeed.setCircleColor(Color.rgb(190, 180, 232));
+        lineDataSetWindSpeed.setColor(Color.rgb(190, 180, 232));
         lineDataSetWindSpeed.setDrawValues(false);
         lineDataSetWindSpeed.setLineWidth(3);
 
+
+        float lineLength = 20;
+        float spaceLength = 30;
+        float phase = 0;
+
+        //        Plot style average temperature
+        LineDataSet lineDataSetMovingAvgTemp = new LineDataSet(yAxesMovingAvgTemp, "Average Temperature " + getString(R.string.degreeC));
+        lineDataSetMovingAvgTemp.setDrawCircles(false);
+        lineDataSetMovingAvgTemp.setColor(Color.argb(216,255, 176, 144));
+        lineDataSetMovingAvgTemp.setDrawValues(false);
+        lineDataSetMovingAvgTemp.setLineWidth(3);
+        lineDataSetMovingAvgTemp.enableDashedLine(lineLength, spaceLength, phase);
+
+//        Plot style average wind speed
+        LineDataSet lineDataSetMovingAvgWindSpeed = new LineDataSet(yAxesMovingAvgWindSpeed, "Average Wind Speed Kph");
+        lineDataSetMovingAvgWindSpeed.setDrawCircles(false);
+        lineDataSetMovingAvgWindSpeed.setColor(Color.argb(216,190, 180, 232));
+        lineDataSetMovingAvgWindSpeed.setDrawValues(false);
+        lineDataSetMovingAvgWindSpeed.setLineWidth(3);
+        lineDataSetMovingAvgWindSpeed.enableDashedLine(lineLength, spaceLength, phase);
+
+//        Add line to graph
         lineDataSets.add(lineDataSetTemp);
         lineDataSets.add(lineDataSetWindSpeed);
+        lineDataSets.add(lineDataSetMovingAvgTemp);
+        lineDataSets.add(lineDataSetMovingAvgWindSpeed);
 
 //        Styling
         lineChart.setData(new LineData(lineDataSets));
-        lineChart.setViewPortOffsets(50, 25, 25, 75);
-
+        lineChart.setViewPortOffsets(75, 25, 75, 75);
         lineChart.getAxisRight().setEnabled(false);
         lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        lineChart.getLegend().setPosition(Legend.LegendPosition.RIGHT_OF_CHART_INSIDE);
+
+        // Legend
+        Legend legend = lineChart.getLegend();
+        legend.setPosition(Legend.LegendPosition.ABOVE_CHART_RIGHT);
+        legend.setForm(Legend.LegendForm.LINE);
+        
+        int[] legendColor = { Color.rgb(255, 176, 144), Color.rgb(190, 180, 232)};
+        String[] legendLabel ={"Temperature", "Wind speed"};
+        List<LegendEntry> legendEntries = new ArrayList<>();
+        for (int i = 0; i < legendColor.length; i++) {
+            LegendEntry entry = new LegendEntry();
+            entry.label = legendLabel[i];
+            entry.form = Legend.LegendForm.LINE;
+            entry.formColor = legendColor[i];
+            legendEntries.add(entry);
+        }
+        legend.setCustom(legendEntries);
+
+
         lineChart.getDescription().setEnabled(false);
         lineChart.setScaleEnabled(false);
 
